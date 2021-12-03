957b2628f530d40fd7c60e91cef61610f30944eb
HiteshKhandelwal901 <hitesh.khandelwal_ug21@ashoka.edu.in>
2021-12-03 15:00:52 +0530

    New ALGO

---
+++ Random_Forest_feature_selection.py
@@ -0,0 +1,16 @@
+#Step 1 : Read the data
+
+#Step 2 : Iterative over feature space starting with all features
+
+         #Step 3 :  Train a random forest on a given feature space
+
+         #Step 4 :  get the gini score for each features
+         # 
+         #Step 5 : Rank them from highest to lowest 
+         #  
+         #Step 6 : eliminate last 5 features
+
+         #Step 7 : Construct the new dataset with the new features 
+
+         #Step 8 : repeat
+          
