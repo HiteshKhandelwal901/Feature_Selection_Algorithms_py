042d3403405a71e496c1229f8a08eabeeae03322
HiteshKhandelwal901 <hitesh.khandelwal_ug21@ashoka.edu.in>
2022-01-12 13:29:58 +0530

    bug fix

--- Black_Hole/BH_run.py
+++ Black_Hole/BH_run.py
@@ -4,6 +4,7 @@ from utility import hamming_score, hamming_scoreCV
 
 
 if __name__ == "__main__":
+    """
     data = pd.read_csv('BH_bipirate_binary_BH_train_test.csv')
 
     Y = data[['Beach','Sunset','FallFoliage','Field','Mountain','Urban']]
@@ -16,4 +17,13 @@ if __name__ == "__main__":
     score, loss = hamming_score(X,Y)
 
     #score, clf, correct, incorrect = hamming_scoreCV(X, Y)
+    print("score = {} loss {}".format(score, loss))
+    """
+
+    data = pd.read_csv('subset_data/Flags_2features.csv')
+    Y = data.iloc[:, -7:]
+    X = data.iloc[:, 1: -7]
+    print(X)
+    print(Y)
+    score, loss = hamming_score(X,Y)
     print("score = {} loss {}".format(score, loss))
